48f3d462282c8965414173d2b64a0be6260139f8
Roberts Krisjanis Plorins <cros1404@gmail.com>
2018-04-11 10:29:37 +0300

    Discover animation

--- js/discover.js
+++ js/discover.js
@@ -1,5 +1,27 @@
+function discoverCardLeave() {
+    var col = document.getElementById("discover-col");
+    col.style.transition = "left 0.195s cubic-bezier(0.4, 0.0, 1, 1)";
+    col.style.left = "-" + window.innerWidth +"px";
+}
+
+function discoverCardEnter() {
+    var col = document.getElementById("discover-col");
+    col.style.opacity = 0;
+    col.style.transition = undefined;
+    col.style.left = window.innerWidth + "px";
+    window.setTimeout(function() {
+        col.style.opacity = 1;
+        col.style.transition = "left 0.225s cubic-bezier(0.0, 0.0, 0.2, 1)";
+        col.style.left = "0px";
+    }, 100);        
+}
+
 ons.ready(function () {
     const starPrecentage = 50;
     const starPercentageRounded = `${(Math.round(starPrecentage / 10) * 10)}%`;
     document.getElementsByClassName("stars-inner")[0].style.width = "50%";
+    document.getElementById("discover-skip-button").onclick = function() {
+        discoverCardLeave();
+        window.setTimeout(discoverCardEnter, 195);
+    };
 });
